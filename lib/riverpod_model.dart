ee5330936edd590b08415af4d1cb9f94be92bc54
P-Lamont <pointlamont@gmail.com>
2025-01-15 10:37:14 +0800

    init repository

---
+++ lib/riverpod_model.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:punkantaan/song_model.dart';
+
+class RiverpodModel extends ChangeNotifier{
+  List<SongModel>? songcontents;
+  int? songIndex;
+  double titleFontSize=20;
+  double bodyFontSize=18;
+  Map<String,int>? highlighted;
+  Future<void> readJson() async {
+    final String response = await rootBundle.loadString('assets/songs.json');
+    final data = await json.decode(response);
+    List<dynamic> contents =data['contents'];
+    songcontents= contents.map((e) => SongModel.fromJson(e)).toList();      
+    notifyListeners();
+  }
+  void setSongIndex(int data){
+    songIndex =data;
+    notifyListeners();
+  }
+  void setTitleSize(double data){
+    titleFontSize =data;
+    notifyListeners();
+  }
+  void setBodySize(double data){
+    bodyFontSize =data;
+    notifyListeners();
+  }
+  void setHighlighted(Map<String,int>? data){
+    highlighted=data;
+    notifyListeners();
+  }
+}
